c8ae485e0d7f5ca9c08ea2b87b7ab494b28f3f85
Nick Peters <nick@barbetsapp.com>
2015-07-08 11:51:20 -0600

    move home to index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root 'invoices#index'
   get 'invoices' => 'invoices#index'
   get 'invoices/:id' => 'invoices#invoice_api'
-  get '/home' => 'invoices#home'
 
 end
