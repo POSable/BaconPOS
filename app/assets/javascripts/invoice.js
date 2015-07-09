29fc35d0f61ee6c0ac3b84fed5e18777ec493d53
David Abramowitz <david.xesllc@gmail.com>
2015-07-09 09:16:15 -0600

    Update API Hash

--- app/assets/javascripts/invoice.js
+++ app/assets/javascripts/invoice.js
@@ -1,17 +1,16 @@
 $(document).ready(function(){
 
   $('.btn-custom').on('click', function(){
+
     var price = $(this).parent().prev().html();
     var item = $(this).parent().prev().prev().prev().html();
-    var add_item = '<td class="col-md-6">' + item + '</td>';
-    var add_price = '<td class="col-md-2">' + price + '</td>';
-    var add_quantity = '<td class="col-md-4">' + 1 + '</td>';
 
-    $('.line_items').after(add_item + add_price + add_quantity);
+    var add_item = '<div class="col-md-6">' + item + '</div>';
+    var add_price = '<div class="col-md-2">' + price + '</div>';
+    var add_quantity = '<div class="col-md-2">' + 1 + '</div>';
+
+    $('#line_items').after('<div class="row text-center">' + add_item + add_price + add_quantity + '</div><hr>');
   })
-
-
-
 
 
 
