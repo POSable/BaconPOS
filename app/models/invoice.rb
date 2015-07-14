06b131bf9c74a98f74dbe7f040f906d06af597af
David Abramowitz <david.xesllc@gmail.com>
2015-07-14 11:09:18 -0600

    Updated Payload

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -10,16 +10,23 @@ class Invoice < ActiveRecord::Base
   def build_invoice_hash
     invoice_hash = {}
     invoice_hash[:customer] = self.customer.name
+    invoice_hash[:customer_id] = self.customer.id
     invoice_hash[:card_type] = self.customer.cc_type
     invoice_hash[:card_last4_digits] = self.customer.cc_last4
     invoice_hash[:authorization_code] = self.customer.cc_auth
-    invoice_hash[:pos_name] = self.pos.name
+    invoice_hash[:quickbooks_id] = 1
+    invoice_hash[:xero_id] = 1
+    invoice_hash[:greatplains_id] = 1
+    invoice_hash[:freshbooks_id] = 1
+    invoice_hash[:netsuite_id] = 1
+    invoice_hash[:posable_invoice_id] = self.id
     transactions = self.transactions
     invoice_hash[:transactions] = {}
     transactions.each do |transaction|
-      invoice_hash[:transactions][transaction.item.id] = {name: transaction.item.name, quantity: transaction.qty, unit_price: transaction.item.price}
+      invoice_hash[:transactions][transaction.id] = {item_id: transaction.item.id, name: transaction.item.name, quantity: transaction.qty, unit_price: transaction.item.price}
     end
     invoice_hash[:total] = self.total
+    invoice_hash[:pos_name] = self.pos.name
     invoice_hash[:date] = self.updated_at.strftime("%m/%d/%Y")
     invoice_hash[:time] = self.updated_at.strftime("%H:%M:%S")
     invoice_hash
@@ -34,11 +41,6 @@ class Invoice < ActiveRecord::Base
                   :record_id => self.id,
                   :token => User.find_by(username: "ce").auth_token,
                   :lastUpdated => self.updated_at.strftime("%m/%d/%Y") + " " + self.updated_at.strftime("%Y-%m-%dT%H:%M:%S"),
-                  :quickbooks_id => 1,
-                  :xero_id => 1,
-                  :greatplains_id => 1,
-                  :freshbooks_id => 1,
-                  :netsuite_id => 1
         }
       )
     end
