efe5b7216d2d871f6de2aa63fae2ad771efc9967
Nick Peters <nick@barbetsapp.com>
2015-07-20 12:18:16 -0600

    add clear button, refactor total logic, edit business info, create add customer option/modal

--- app/assets/javascripts/invoice.js
+++ app/assets/javascripts/invoice.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
 
-    $('.btn-custom').on('click', function(){
+    $('.btn-item').on('click', function(){
       var allItems = [];
       var total = 0;
-
       var price = $(this).parent().prev().children('#price').html();
       var item = $(this).parent().parent().prev().prev().prev().children('#name').html();
       var id = $(this).parent().attr('id');
@@ -34,10 +33,15 @@ $(document).ready(function(){
         var trashItem = $(this).parent().parent();
         var divider = trashItem.next();
         var trashPrice = trashItem.children('.item_price').html();
+        var newTotal = total - trashPrice;
         trashItem.remove();
         divider.remove();
-        var newTotal = total - trashPrice;
-        $('#total').text(newTotal.toFixed(2));
+        if ($('.item_price').length == 0) {
+          var empty = 0;
+          $('#total').text(empty.toFixed(2));
+        } else {
+          $('#total').text(newTotal.toFixed(2));
+        }
       })
 
       $('.item_price').each(function(){
@@ -45,7 +49,7 @@ $(document).ready(function(){
       })
       $('#total').text(total.toFixed(2));
 
-    })
+    });
 
     $('#pay').on('click', function(e){
 
@@ -54,29 +58,40 @@ $(document).ready(function(){
       $('#invoice_list #item').each(function(){
         var itemID = $(this).children('.item_name').attr('id');
         var quantity = $(this).children('.item_qty').html();
-
         invoiceTrans.push({"item_id": itemID, "item_qty": quantity});
-
       })
-
       var test = JSON.stringify(invoiceTrans)
-
       $.ajax({
         type: 'POST',
         url: "invoice/ajax_create",
         traditional: true,
         dataType: 'json',
         data: {"transactions": test, "customer_id": 1, "pos_id": 1},
-
         error: function(error){
           console.log(error);
         },
         success: function(new_invoice){
           console.log(new_invoice);
         }
-
       });
       e.preventDefault();
+    })
+
+    $('#add_customer').on('click', function(){
+      var newCustomer = $('#new_name').val();
+      $('#select').append('<option>' + newCustomer + '</option>');
+      $('#new-customer').toggleClass('in');
+      $('#new-customer').css('display', 'none');
+      $('.modal-backdrop').hide();
+    })
+
+    $('#clear').on('click', function(){
+      $('#invoice_list #item').each(function(){
+        var reset = 0;
+        $(this).next().remove();
+        $(this).remove();
+        $('#total').text(reset.toFixed(2));
+      })
     })
 
 });
